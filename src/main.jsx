a6dcc463fca5be9d3fe51a86dba83f1f979f099c
jsprogram2014 <jsprogram2014@gmail.com>
2025-08-10 17:09:59 -0400

    fix for running

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import './styles/globals.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/Movie-Review-App">
         <App />
       </BrowserRouter>
     </Provider>
